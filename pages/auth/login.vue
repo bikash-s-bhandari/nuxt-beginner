6565eb0d8c34a99ed0cc4f60c619c7c4626ebcb2
bikash bhandari <bikas@thefireflytech.com>
2024-08-24 12:11:28 +0545

    nested page

---
+++ pages/auth/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    login page
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/post/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    this is post details with id {{ $route.params.id }} {{ route.params.id }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute();
+</script>
+
+<style></style>
